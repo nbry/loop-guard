7c458f2c03af80c29a7d8ba998fac8f787a59467
nbry <nwbryant@umich.edu>
2022-06-26 20:59:01 -0700

    chore: first draft!

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+/// Utility for preventing infinite loops
+///
+/// Use as a guard rail inside a `loop` or `while` block
+///
+/// Meant to be used as a development dependency
+pub struct LoopGuard {
+    pub max_ticks: i32,
+    pub count: i32,
+}
+
+impl LoopGuard {
+    /// # Create a new LoopGuard
+    /// Make sure to create the LoopGuard instance **outside** the loop block
+    ///
+    /// ## Arguments
+    /// * `max_ticks` - The limit of how many times a loop should run
+    ///
+    /// ## Example
+    /// ```
+    /// let guard = LoopGuard::new(100);
+    ///
+    /// loop {
+    ///     guard.protect() // This panic after 10 loops
+    /// }
+    /// ```
+    pub fn new(max: i32) -> LoopGuard {
+        LoopGuard {
+            max_ticks: max,
+            count: 0,
+        }
+    }
+
+    /// From within a `while` or `loop` block, panic if the loop iteration
+    /// surpasses the LoopGuard's `max_ticks` value.
+    /// ```
+    /// ...
+    /// loop {
+    ///     guard.protect() // This panic after 10 loops
+    /// }
+    /// ```
+    pub fn protect(&mut self) {
+        self.count += 1;
+        if self.count > self.max_ticks {
+            panic!("Max number of ticks reached");
+        }
+    }
+}
+
+#[test]
+#[should_panic]
+fn loop_goes_past_max_ticks() {
+    // Arrange
+    let mut guard = LoopGuard::new(1000);
+
+    // Act
+    loop {
+        // Infinite loop - should obviously panic
+        guard.protect();
+    }
+}
